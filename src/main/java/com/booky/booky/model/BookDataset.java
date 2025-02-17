860c9538431b10e9ce677a5512fae7265cdce79a
Ester <ester.vasquez.escudero@gmail.com>
2025-02-17 00:37:50 -0600

    add entity for return 500 results

---
+++ src/main/java/com/booky/booky/model/BookDataset.java
@@ -0,0 +1,44 @@
+package com.booky.booky.model;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Immutable;
+
+
+@Entity
+@Table(name = "booksdataset_clean")
+@Immutable
+public class BookDataset {
+    @Id
+    private String title;
+    private String authors;
+    private String description;
+    private String category;
+    private String pubisher;
+    private String publish_date;
+    private String price;
+
+    public BookDataset() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthors() {
+        return authors;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getPubisher() {
+        return pubisher;
+    }
+    public String getPublish_date() {
+        return publish_date;
+    }
+    public String getPrice() {
+        return price;
+    }
+
+}
