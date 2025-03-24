645cf3a9c9a95006afac6ae9314da6c56ddf78bb
Diego Jacinto <diego.jacinto.ibarra@gmail.com>
2025-03-23 20:33:49 -0600

    clean up BookController imports and update home.html to reflect query parameter in endpoint

--- src/main/java/com/booky/booky/controller/BookController.java
+++ src/main/java/com/booky/booky/controller/BookController.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.booky.booky.service.BookService;
 import com.booky.booky.model.Book;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
