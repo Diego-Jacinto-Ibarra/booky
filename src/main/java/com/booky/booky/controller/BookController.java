c07318f073b4142e8aaa0f76cbb72caa5f00777c
Ester <ester.vasquez.escudero@gmail.com>
2025-03-26 01:25:11 -0600

    feat: add book retrieval functionality to include filtering by authors, category, and publisher

--- src/main/java/com/booky/booky/controller/BookController.java
+++ src/main/java/com/booky/booky/controller/BookController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.booky.booky.service.BookService;
+
 import com.booky.booky.model.Book;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -22,10 +21,12 @@ public class BookController {
     @GetMapping("/books")
     public List<Book> listarProductos(
             @RequestParam(required = false) String title,
-            @RequestParam(required = false) Double price) {
+            @RequestParam(required = false) Float price,
+            @RequestParam(required = false) String authors,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String publisher){
 
-        bookService.findBooksByFilter(title, price);
 
-        return bookService.getAllBooks();
+        return bookService.findBooksByFilter(title, price,authors,category,publisher);
     }
 }
--- src/main/java/com/booky/booky/service/BookService.java
+++ src/main/java/com/booky/booky/service/BookService.java
@@ -6,6 +6,7 @@ import com.booky.booky.model.Book;
 import com.booky.booky.repository.BookRepository;
 
 import java.util.List;
+import java.util.concurrent.Flow.Publisher;
 
 
 @Service
@@ -15,24 +16,44 @@ public class BookService {
     private BookRepository bookRepository;
 
     public List<Book> getBooks() {
-        return bookRepository.findAll().stream().limit(500).toList();
+        return bookRepository.findAll().stream().limit(5).toList();
     }
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-    public List<Book> findBooksByFilter(String title, double price) {
+    public List<Book> findBooksByFilter(String title, Float price, String authors, String category, String publisher) {
         List<Book> books = getAllBooks();
 
+        if (title == null && price == null && authors == null && category == null && publisher == null) {
+            return getBooks();
+        }
         if (title != null) {
             books = books.stream().filter(book -> book.getTitle().equals(title)).toList();
         }
 
-        if (price != 0) {
-            books = books.stream().filter(book -> book.getPrice() == price).toList();
+        if (price != null) {
+            books = books.stream().filter(book -> Float.compare(book.getPrice(),price)==0).toList();
+
+        }
+        if (authors != null) {
+            books = books.stream().filter(book -> book.getAuthors() == authors).toList();
         }
 
+        if (category != null) {
+            books = books.stream().filter(book -> book.getCategory() == category).toList();
+        }
+
+        if (publisher != null) {
+            books = books.stream().filter(book -> book.getPublisher() == publisher).toList();
+        }
+
+        if (books.size() == 0) {
+            System.out.println("No books found");
+            return null;
+
+        }
         return books;
 
     }
